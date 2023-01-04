1dc8a78290b4dd36d1d198f5dd75799f167f29f6
guilhermedcorrea <78979736+guilhermedcorrea@users.noreply.github.com>
2023-01-04 04:08:04 -0300

    app

--- app/models/google_shopping.py
+++ app/models/google_shopping.py
@@ -4,3 +4,14 @@ db = SQLAlchemy()
 def google_db(app):
     db.init_app(app)
     app.db = db
+
+
+class UrlBase(db.Model):
+    __tablename__ = "urlbase"
+    __bind_key__ = 'comparador'
+    __table_args__ = {"schema": "precos"}
+
+class ComparadorPreco(db.Model):
+    __tablename__ = "googleprecos"
+    __bind_key__ = 'comparador'
+    __table_args__ = {"schema": "precos"}
