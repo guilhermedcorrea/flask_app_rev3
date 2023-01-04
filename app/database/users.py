f72fec8caa0878e86aa156086926e994e8745e68
guilhermedcorrea <78979736+guilhermedcorrea@users.noreply.github.com>
2023-01-04 01:08:06 -0300

    app

---
+++ app/database/users.py
@@ -0,0 +1,31 @@
+
+
+class Usuarios:
+    idusuario = ''
+    nomeusuario = ''
+    email = ''
+    senha = ''
+    grupo_usuario = ''
+    usuario_ativo = ''
+    datacadastro = ''
+    data_login = ''
+
+class GrupoUsuarios:
+    idgrupo = 
+    grupo = 
+    data_cadastro = ''
+
+class LogUsuarios:
+    idlog = ''
+    data_logado = ''
+    dia_logado = ''
+    hora_logado = ''
+    visualizacao = ''
+    tempo_visualizacao = ''
+    idusuario = ''
+    id_grupo = ''
+
+
+
+    
+
